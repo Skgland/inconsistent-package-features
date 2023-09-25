1e2755c517b6e849014351c2d9563b234ace1c96
Skgland <bennet.blessmann+github@googlemail.com>
2023-09-25 20:29:49 +0200

    add ci matrix and properly set exit code

--- test_root_package.sh
+++ test_root_package.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 pushd root-package
 cargo +beta build --workspace --no-default-features --features=replace-b
 popd
--- test_virtual_workspace.sh
+++ test_virtual_workspace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 pushd virtual-workspace
 cargo +beta build --workspace --no-default-features --features=replace-b
 popd
